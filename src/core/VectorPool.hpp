2406a6500aba4a8b2f9e97be83fc7f971c963cc3
mxmlnkn <mxmlnkn@github.de>
2024-01-26 01:09:52 +0100

    also does not work

--- src/core/VectorPool.hpp
+++ src/core/VectorPool.hpp
@@ -107,26 +107,32 @@ class VectorPool :
     public std::enable_shared_from_this<VectorPool<Container> >
 {
 public:
-    class WrappedContainer :
-        private Container
+    class WrappedContainer
     {
+    public:
+        using const_iterator = typename Container::const_iterator;
+
     public:
         WrappedContainer() = default;
         /* This destructor definition leads to a memory leak resulting in an OOM kill after 70 GB! */
-        ~WrappedContainer() = default;
+        ~WrappedContainer() {}
 
-        using Container::data;
-        using Container::begin;
-        using Container::end;
-        using Container::rbegin;
-        using Container::rend;
-        using Container::reserve;
-        using Container::capacity;
-        using Container::size;
-        using Container::resize;
-        using Container::shrink_to_fit;
-        using Container::insert;
-        using Container::operator[];
+        [[nodiscard]] auto data() { return m_container.data(); };
+        [[nodiscard]] auto data() const { return m_container.data(); };
+        [[nodiscard]] auto begin() const { return m_container.begin(); };
+        [[nodiscard]] auto end() const { return m_container.end(); };
+        [[nodiscard]] auto rbegin() const { return m_container.rbegin(); };
+        [[nodiscard]] auto rend() const { return m_container.rend(); };
+        [[nodiscard]] auto capacity() const { return m_container.capacity(); };
+        [[nodiscard]] auto size() const { return m_container.size(); };
+        void reserve( size_t newCapacity ) { return m_container.reserve( newCapacity ); };
+        void resize( size_t newSize ) { return m_container.resize( newSize ); };
+        void shrink_to_fit() { return m_container.shrink_to_fit(); };
+        template<class InputIt>
+        auto insert( const_iterator pos, InputIt first, InputIt last ) { return m_container.insert( pos, first, last ); };
+        [[nodiscard]] auto operator[]( size_t index ) const { return m_container.operator[]( index ); };
+    private:
+        Container m_container;
     };
 
     struct Statistics
